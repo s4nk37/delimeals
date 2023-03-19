5dc5fb03015a52b7f9e1f97629719dfd53272463
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2023-03-19 19:34:46 +0530

    Added categories_screen.dart ,fonts and colorscheme

---
+++ lib/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'category_item.dart';
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+        elevation: 0.0,
+      ),
+      body: GridView(
+          padding: const EdgeInsets.all(15.0),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: size.width / 2,
+              childAspectRatio: 3 / 2.2,
+              crossAxisSpacing: size.width / 40,
+              mainAxisSpacing: size.width / 40),
+          children: dummyCategories.map((category) {
+            return CategoryItem(title: category.title, color: category.color);
+          }).toList()),
+    );
+  }
+}
